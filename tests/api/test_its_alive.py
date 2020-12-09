fcb64dfc797322b4acb083783e1eee2113a1a692
Victor Pereira <victtor.vpb@gmail.com>
2020-12-09 14:27:55 -0300

    fix tests its_alive

--- tests/api/test_its_alive.py
+++ tests/api/test_its_alive.py
@@ -1,11 +1,11 @@
+from fastapi import status
 from fastapi.testclient import TestClient
-from http import HTTPStatus
 
 
 def test_its_alive(client: TestClient):
     data = {'status': 'its_alive'}
     response = client.get('its-alive')
 
-    assert response.status_code == HTTPStatus.OK
+    assert response.status_code == status.HTTP_200_OK
 
     assert response.json() == data
