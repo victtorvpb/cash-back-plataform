c2a7239168d59c353fbb00b6c0f5e642cdb368ad
Victor Pereira <victtor.vpb@gmail.com>
2020-12-13 21:23:18 -0300

    fix test login

--- tests/api_v1/tests_login.py
+++ tests/api_v1/tests_login.py
@@ -9,7 +9,7 @@ def test_get_access_token(db: Session, client: TestClient) -> None:
 
     dict_user = create_random_user(db)
     login_data = {
-        "email": dict_user.get("email"),
+        "username": dict_user.get("email"),
         "password": dict_user.get("password"),
     }
     response = client.post(f"{settings.API_V1_STR}/login/access-token", data=login_data)
