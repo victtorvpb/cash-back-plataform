797bad57d081118189170857559267523563b96c
Victor Pereira <victtor.vpb@gmail.com>
2020-12-14 11:30:51 -0300

    add credit test

--- tests/api_v1/tests_purchae.py
+++ tests/api_v1/tests_purchae.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 from typing import Generator
 from fastapi.testclient import TestClient
+from fastapi import status
 from sqlalchemy.orm import Session
 
 from commons import crud, schemas
@@ -46,3 +47,34 @@ def test_post_new_purchase(
     assert purchase
     assert float(purchase.cashback_percente) == percente_cashback
     assert float(purchase.cashback_value) == value * (percente_cashback / 100)
+
+
+def test_get_credit(
+    db: Session, client: TestClient, faker: Generator) -> None:
+
+    dict_user = create_random_user(db)
+    user = dict_user.get('user')
+    cpf = user.cpf
+ 
+    headers = user_authentication_headers(client, user.email, dict_user.get('password'))
+    response = client.get(f"{settings.API_V1_STR}/credit-delear?reseller_cpf={cpf}", headers=headers)
+
+    assert 200 <= response.status_code < 300
+
+    created_purchase = response.json()
+
+    assert schemas.PurcahseCredit.parse_obj(created_purchase).credit
+
+
+def test_get_credit_whitout_user(
+    db: Session, client: TestClient, faker: Generator) -> None:
+
+    dict_user = create_random_user(db)
+    user = dict_user.get('user')
+    cpf = user.cpf
+ 
+    headers = user_authentication_headers(client, user.email, dict_user.get('password'))
+    response = client.get(f"{settings.API_V1_STR}/credit-delear?reseller_cpf=teste", headers=headers)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
